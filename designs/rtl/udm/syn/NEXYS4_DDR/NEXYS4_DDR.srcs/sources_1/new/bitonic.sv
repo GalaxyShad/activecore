c9589ea0c99ab0f02713647bef2cdf206681e1ef
GalaxyShad <vadim.kadyrin@mail.ru>
2024-11-14 23:40:56 +0300

    [~] lab 3?

--- designs/rtl/udm/syn/NEXYS4_DDR/NEXYS4_DDR.srcs/sources_1/new/bitonic.sv
+++ designs/rtl/udm/syn/NEXYS4_DDR/NEXYS4_DDR.srcs/sources_1/new/bitonic.sv
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Create Date: 07.11.2024 16:16:36
@@ -7,72 +8,87 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module Bitonic #(parameter LIST_SIZE = 8, parameter LIST_VALUE_BIT_COUNT = 32) (
-          input  [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] original_list_i
-        , output [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] sorted_list_o
-    );
-    
-localparam UINT32_SIZE = 32;
+module Bitonic #(parameter LIST_SIZE = 8, parameter LIST_VALUE_BIT_COUNT = 32) (         
+    input  [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] original_list_i
+    , output [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] sorted_list_o        
+    , input clk_i
+    , input rst_i    
+);
 
-logic [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] buffer_list;
+logic [LIST_SIZE-1:0][LIST_VALUE_BIT_COUNT-1:0] buffer;
+logic [3:0] stage = 0;
 
-assign sorted_list_o = buffer_list;
-
-function void compare_and_swap(
+function void compare_and_swap(    
     inout logic [LIST_VALUE_BIT_COUNT-1:0] a,
     inout logic [LIST_VALUE_BIT_COUNT-1:0] b
 );
-    logic[LIST_VALUE_BIT_COUNT-1:0] temp;
+    logic[LIST_VALUE_BIT_COUNT-1:0] temp;    
     if (a > b) begin
         temp = a;
-        a = b;
+        a = b;        
         b = temp;
     end
 endfunction
 
-always_comb begin
-    // copy to buffer
-    buffer_list = original_list_i;
-    
-    // # =========================
-    // # stage 1
-    // # =========================
-    
-    compare_and_swap(buffer_list[0], buffer_list[1]); // # -> 
-    compare_and_swap(buffer_list[3], buffer_list[2]); // # <-
-    compare_and_swap(buffer_list[4], buffer_list[5]); // # ->
-    compare_and_swap(buffer_list[7], buffer_list[6]); // # <- 
-        
-    // # =========================
-    // # stage 2
-    // # =========================
-    compare_and_swap(buffer_list[0], buffer_list[2]); // # ->
-    compare_and_swap(buffer_list[1], buffer_list[3]); // # ->
-    compare_and_swap(buffer_list[6], buffer_list[4]); // # <-
-    compare_and_swap(buffer_list[7], buffer_list[5]); // # <-
-    
-    compare_and_swap(buffer_list[0], buffer_list[1]); // # ->
-    compare_and_swap(buffer_list[2], buffer_list[3]); // # ->
-    compare_and_swap(buffer_list[5], buffer_list[4]); // # <-
-    compare_and_swap(buffer_list[7], buffer_list[6]); // # <-
-        
-    // # =========================
-    // # stage 3
-    // # =========================
-    compare_and_swap(buffer_list[0], buffer_list[4]); // # ->
-    compare_and_swap(buffer_list[1], buffer_list[5]); // # ->
-    compare_and_swap(buffer_list[2], buffer_list[6]); // # ->
-    compare_and_swap(buffer_list[3], buffer_list[7]); // # ->
-    
-    compare_and_swap(buffer_list[0], buffer_list[2]); // # ->
-    compare_and_swap(buffer_list[1], buffer_list[3]); // # ->
-    compare_and_swap(buffer_list[4], buffer_list[6]); // # ->
-    compare_and_swap(buffer_list[5], buffer_list[7]); // # ->
-    
-    compare_and_swap(buffer_list[0], buffer_list[1]); // # ->
-    compare_and_swap(buffer_list[2], buffer_list[3]); // # ->
-    compare_and_swap(buffer_list[4], buffer_list[5]); // # ->
-    compare_and_swap(buffer_list[6], buffer_list[7]); // # ->
+always @(posedge clk_i) begin    
+    if (rst_i)              stage <= 0;
+    else if (stage == 7)    stage <= 0;
+    else                    stage <= stage + 1;
 end
 
+always @* begin    
+    case (stage)
+        0: buffer = original_list_i;
+            
+        1: begin            
+            compare_and_swap(buffer[0], buffer[1]); // # -> 
+            compare_and_swap(buffer[3], buffer[2]); // # <-            
+            compare_and_swap(buffer[4], buffer[5]); // # ->
+            compare_and_swap(buffer[7], buffer[6]); // # <-         
+        end
+
+
+        // STAGE 2
+        2: begin            
+            compare_and_swap(buffer[0], buffer[2]); // # ->
+            compare_and_swap(buffer[1], buffer[3]); // # ->            
+            compare_and_swap(buffer[6], buffer[4]); // # <-
+            compare_and_swap(buffer[7], buffer[5]); // # <-        
+        end
+
+        3: begin
+            compare_and_swap(buffer[0], buffer[1]); // # ->            
+            compare_and_swap(buffer[2], buffer[3]); // # ->
+            compare_and_swap(buffer[5], buffer[4]); // # <-            
+            compare_and_swap(buffer[7], buffer[6]); // # <- 
+        end        
+
+
+        // STAGE 3        
+        4: begin
+            compare_and_swap(buffer[0], buffer[4]); // # ->            
+            compare_and_swap(buffer[1], buffer[5]); // # ->
+            compare_and_swap(buffer[2], buffer[6]); // # ->            
+            compare_and_swap(buffer[3], buffer[7]); // # ->
+        end   
+
+        5: begin            
+            compare_and_swap(buffer[0], buffer[2]); // # ->
+            compare_and_swap(buffer[1], buffer[3]); // # ->            
+            compare_and_swap(buffer[4], buffer[6]); // # ->
+            compare_and_swap(buffer[5], buffer[7]); // # ->        
+        end
+
+        6: begin
+            compare_and_swap(buffer[0], buffer[1]); // # ->            
+            compare_and_swap(buffer[2], buffer[3]); // # ->
+            compare_and_swap(buffer[4], buffer[5]); // # ->            
+            compare_and_swap(buffer[6], buffer[7]); // # -> 
+        end    
+
+        7: begin
+            sorted_list_o = buffer;
+        end
+    endcase
+end 
 endmodule
